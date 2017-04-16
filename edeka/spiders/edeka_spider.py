0ed19164356d22df71c9decc603090b3513f4c51
simba999 <simba999@github.com>
2017-04-16 01:32:41 -0700

    Detail product page scrapy

--- edeka/spiders/edeka_spider.py
+++ edeka/spiders/edeka_spider.py
@@ -27,24 +27,40 @@ class EdekaSpiderSpider(scrapy.Spider):
 	name = "edeka"
 	allowed_domains = ["edekanord-shop.de"]
 	start_urls = ['http://edekanord-shop.de/']
-	domain = 'http://edekanord-shop.de/boecker'
+	domain = 'http://edekanord-shop.de'
 
 	def parse(self, response):
-		with open('response.html', 'w') as f:
-			f.write("ab")
-			f.close()
-		request = scrapy.Request(self.domain, callback=self.parse_shop)
+		product_thumb_url = self.domain + '/boecker/products?query=3090291386041'
+		request = scrapy.Request(product_thumb_url, callback=self.parse_shop)
 		return request
 
-	def parse_shop(self, response, id):
-		product_thumb_page = self.domain + 'products?query=3090291386041'
-
+	def parse_shop(self, response, id=0):
+		
 		body_result = response.xpath("//body").extract()
-		print "%%%%%%%%%%%%%%%%%%% ", " : ", type(body_result[0]), "%%%%%%%%%%%%%%%%%%%"
-
-		# result = body_result[0]
-		# unicodedata.normalize('NFKD', result).encode('utf-8')
-
+		
 		f = open('response.html', 'w')
 		f.write(body_result[0].encode('utf-8'))
 		f.close()
+
+		body_content = response.xpath("//body")
+
+		product_detail_url = body_content.xpath(".//div[contains(@class, 'product-list')]//a[contains(@class, 'img-holder')]/@href").extract()
+		print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+		print self.domain + product_detail_url[0]
+		print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+		request = scrapy.Request(self.domain + product_detail_url[0], callback=self.parse_product_detail)
+		yield request
+
+	def parse_product_detail(self, response):
+		detail_result = response.xpath("//body").extract()
+
+		f = open('detail_esponse.html', 'w')
+		f.write(detail_result[0].encode('utf-8'))
+		f.close()
+
+
+		print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+		print detail_content
+		print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+		
